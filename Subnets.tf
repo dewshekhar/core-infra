a3290e2fafdd2e37a93cb42d048592899c2a2e09
dewshekhar <45204261+dewshekhar@users.noreply.github.com>
2022-08-05 20:32:22 +0530

    Update Subnets.tf

--- Subnets.tf
+++ Subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "pub_sub1" { 
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.pub_sub_cidr_1
-  availability_zone       = "us-west-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     Name                     = "${lower(var.app_name)}-pub-sub-1"
@@ -11,7 +11,7 @@ resource "aws_subnet" "pub_sub1" {
 resource "aws_subnet" "pub_sub2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.pub_sub_cidr_2
-  availability_zone       = "us-west-1c"
+  availability_zone       = "us-east-1c"
   map_public_ip_on_launch = true
   tags = {
     Name = "${lower(var.app_name)}-pub-sub-2"
@@ -22,7 +22,7 @@ resource "aws_subnet" "pub_sub2" {
 resource "aws_subnet" "pri_sub1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.pri_sub_cidr_1
-  availability_zone = "us-west-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "${lower(var.app_name)}-pri-1"
   }
@@ -31,7 +31,7 @@ resource "aws_subnet" "pri_sub1" {
 resource "aws_subnet" "pri_sub2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.pri_sub_cidr_2
-  availability_zone = "us-west-1c"
+  availability_zone = "us-east-1c"
   tags = {
     Name = "${lower(var.app_name)}-pri-2"
   }
