4834acaec5518b3f788254484c65bd2524c89675
dewshekhar <45204261+dewshekhar@users.noreply.github.com>
2022-09-17 12:44:31 +0530

    Update Variables.tf

--- Variables.tf
+++ Variables.tf
@@ -1,23 +1,29 @@
 variable "app_name" {
 type = string
+default="Test"
 }
 
 variable "vpc_cidr" {
     type = string
+default = "10.0.0.0/16"
 }
 
 variable "pub_sub_cidr_1" {
     type = string
+default ="10.0.0.0/18"
 }
 
 variable "pub_sub_cidr_2" {
     type = string
+default = "10.0.64.0/18"
 }
 
 variable "pri_sub_cidr_1" {
     type = string
+default = "10.0.128.0/18"
 }
 
 variable "pri_sub_cidr_2" {
     type = string
+default ="10.0.192.0/18"
 }
